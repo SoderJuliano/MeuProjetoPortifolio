0a3e44683033cbaad199a780f274f94d1769165c
Juliano <julianosoder1989@gmail.com>
2021-08-06 17:50:30 -0300

    fix print bnt on internet explorer

--- src/components/Imprimir-bnt.vue
+++ src/components/Imprimir-bnt.vue
@@ -9,6 +9,8 @@ export default {
     name: 'imprimir-bnt',
     methods:{
         print(){
+            window.document.close();
+            window.focus();
             window.print()
         },
     }
