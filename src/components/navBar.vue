9f1c8a0d980ec0d4c32322b5e73298439c163cc9
Juliano Soder <julianosoder1989@gmail.com>
2021-11-26 23:11:49 -0300

    new changes new menu

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,46 +1,61 @@
 <template>
     <div class="container-fluid">
         <nav class="navbar">
-         <!-- <imprimir
-                  class="navbar-toggler"
-                /> -->
-           <ul class="nav no-search">
-             <li v-on:click="imprimir" class="nav-item" id="inprimir-item"><a href="#">IMPRIMIR</a></li>
-              <li class="nav-item" id="navbarDropdown">
-                <a class="nav-link" href="#" >
-                  Informações
-                </a>
-                <div class="dropdown-menu" >
-                  <a v-on:click="about" class="dropdown-item" href="#">Sobre o Site</a>
-                  <a v-on:click="more" class="dropdown-item" href="#">Mais Exemplos</a>
-                  <a v-on:click="support" class="dropdown-item" href="#">Apoiar este Projeto</a>
-                </div>
-              </li>
+          <ul class="nav no-search">
+            <div class="nav-item is-right">
+                <img @click="showMenu" src="../icons/menustatic.png" alt="menu">
+            </div>
+            
+            
+              <div class="right-options">
+                  <li v-on:click="imprimir" class="nav-item" id="inprimir-item"><a href="#">IMPRIMIR</a></li>
+                    <li class="nav-item" id="navbarDropdown">
+                      <a class="nav-link" href="#" >
+                        Informações
+                      </a>
+                      <div class="dropdown-menu" >
+                        <a v-on:click="about" class="dropdown-item" href="#">Sobre o Site</a>
+                        <a v-on:click="more" class="dropdown-item" href="#">Mais Exemplos</a>
+                        <a v-on:click="support" class="dropdown-item" href="#">Apoiar este Projeto</a>
+                      </div>
+                    </li>
 
-               
-              <li class="nav-item" id="navbarDropdown">
-                <a class="nav-link" href="#" >
-                  Sobre mim
-                </a>
-                 <div class="dropdown-menu" >
-                  <li v-on:click="aboutMe" class="nav-item"><a href="#">Sobre Mim</a></li>
-                  <li v-on:click="contact" class="nav-item"><a href="#">Entre em contato</a></li>
+                    
+                    <li class="nav-item" id="navbarDropdown">
+                      <a class="nav-link" href="#" >
+                        Sobre mim
+                      </a>
+                      <div class="dropdown-menu" >
+                        <li v-on:click="aboutMe" class="nav-item"><a href="#">Sobre Mim</a></li>
+                        <li v-on:click="contact" class="nav-item"><a href="#">Entre em contato</a></li>
+                      </div>
+                    </li>
                 </div>
-              </li>
+              
             </ul>
         </nav> 
     </div>
-  
+  <options-menu 
+    :show = show
+  />
 </template>
 
 <script>
-//import imprimir from './Imprimir-bnt.vue'
+import optionsMenu from './optionsMenu.vue'
 export default {
     name: 'nav-bar',
     components:{
-     // imprimir
+      optionsMenu
+    },
+    data(){
+      return{
+        show: false
+      }
     },
     methods:{
+      showMenu(){
+        this.show = true
+      },
       about(){
         alert("Este site foi desenvolvido para suprir a necessidade de montar cvs ou atualizar currículos de forma rápida, pratica e de forma gratuíta, gerando um cv bonito e profissional altamente configurável.");
         alert("Este site não utiliza cookies e também não guarda nenhuma informação pessoal. Todas as suas informações estão no cache do navegador e no seu localStorage, para apagá-los, deve apagar os dados do navegador.")
@@ -80,6 +95,7 @@ export default {
   justify-content: space-around;
   float: right;
   margin: 0 auto;
+  width: 100%;
 }
 .navbar ul {
   list-style: none;
@@ -123,5 +139,17 @@ export default {
 }
 @media only screen and (max-width: 720px) { 
  .container-fluid{display: none;}
+}
+.nav-item.is-right{
+  position: absolute;
+  width: 20%;
+  left: 20%;
+  display: flex;
+}
+.right-options{
+  position: absolute;
+  width: 20%;
+  right: 20%;
+  display: flex;
 }
 </style>
