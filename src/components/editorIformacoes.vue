67945c964f4bcb355aef3c2da5ffc3352b95302e
Juliano Soder <juliano.soder@inova.ind.br>
2021-12-22 10:10:04 -0300

    making modal component

---
+++ src/components/editorIformacoes.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="main-editar-container">
+        <h3>{{title}}</h3>
+        <div class="body-editar-container">
+            <form action="">
+                <div for="inputsNumber">
+                    <div v-if="formTitle1">
+                    <input type="text" placeholder="{{placeholder1}}">
+                </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
