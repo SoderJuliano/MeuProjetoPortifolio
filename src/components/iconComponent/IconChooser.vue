8ae8b8a871f01aa7d6e6a208ed750999029b5347
Juliano <julianosoder1989@gmail.com>
2023-02-16 22:23:28 -0300

    initing iconChooser

---
+++ src/components/iconComponent/IconChooser.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'iconChooser',
+    methods: {
+
+    },
+    props: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/iconComponent/emailIcons.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img src="../../icons/envelope.svg" class="email-icon"/>
+        <img src="../../icons/email/email.png" class="email-icon"/>
+        <img src="../../icons/email/email2.png" class="email-icon"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'email',
+    methods: {
+
+    },
+    props: {
+        
+    }
+}
+</script>
