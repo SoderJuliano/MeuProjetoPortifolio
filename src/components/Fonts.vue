5c62fea89b9050b9f8388bf13e606598e54719e7
Juliano Soder <julianosoder1989@gmail.com>
2024-04-03 00:02:33 -0300

    fix Footer and improving options menu css

--- src/components/Fonts.vue
+++ src/components/Fonts.vue
@@ -90,14 +90,16 @@ export default {
 .fonts-container{
   height: 40px;
 }
-.closeFontsDiv{
+.closeFontsDiv {
+  text-align: center;
+  position: relative;
+  width: 40px;
+  padding: 10px;
+  left: 70%;
   border-radius: 5px;
   border: solid 1px black;
-  width: 20px;
-  height: 20px;
-  text-align: center;
-  font-size: 16px !important;
-  background-color: black;
+  font-size: 20px !important;
+  background-color: red;
   color: white;
 }
 
--- src/components/PictureShape.vue
+++ src/components/PictureShape.vue
@@ -2,11 +2,15 @@
     <div class="shape-container">
         <p class="tside" @click="openContainer()">TIPO DE IMAGEM CONTAINER</p>
         <div class="shapes">
-            <div class="closeDiv" @click="closeDiv()">X</div>
-            <div id="square" class="square"></div>
-            <div id="triangleUp" class="triangle-up"></div>
-            <div id="circle" class="circle"></div>
-            <div id="colorfull-circle" class="colorfull-circle"></div>
+            <div>
+                <div class="closeDiv" @click="closeDiv()">X</div>
+            </div>
+            <div class="shapes-itens">
+                <div id="square" class="square item"></div>
+                <div id="triangleUp" class="triangle-up item"></div>
+                <div id="circle" class="circle item"></div>
+                <div id="colorfull-circle" class="colorfull-circle item"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,7 +30,7 @@ export default {
         },
         openContainer(){
             this.closeAll();
-            $(".shapes").css({"display": "flex", "position": "absolute"});
+            $(".shapes").css({"display": "block", "position": "absolute"});
         },
         closeDiv(){
             $(".shapes").css({"display": "none"});
@@ -36,9 +40,27 @@ export default {
 </script>
 <style scoped>
 
-.shapes{
+.shapes-itens {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    margin-top: 20px;
+}
+
+.shapes-itens .item:active {
+    background-color: white;
+    transform: scale(.9);
+}
+
+.item {
+    width: 40px;
+    height: 40px;
+}
+
+.shapes 
+{
     padding: 15px;
-    height: 100px;
+    min-height: 100px;
     width: 300px;
     border-style: solid;
     border-color: black;
@@ -48,14 +70,11 @@ export default {
     justify-content: space-around;
     display: none;
 }
+
 .square {
-  width: 25px;
-  height: 25px;
   border: 2px solid black;
 }
 .colorfull-circle {
-  width: 25px;
-  height: 25px;
   border-radius: 50%;
   border-top: 5px solid rgb(255, 2, 2);
   border-left: 5px solid rgb(68, 0, 255);
@@ -63,14 +82,10 @@ export default {
   border-bottom: 5px solid rgb(255, 0, 221);
 }
 .circle {
-  width: 25px;
-  height: 25px;
   border-radius: 50%;
   border: 2px solid black;
 }
 .triangle-up {
-    width: 25px;
-    height: 25px;
     border: 2px solid black;
     border-radius: 11px;
 }
