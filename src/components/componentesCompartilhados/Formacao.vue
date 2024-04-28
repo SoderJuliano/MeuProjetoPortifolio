c84a77f406cdf7be7ee0e70356b7e43731465d06
Juliano Soder <julianosoder1989@gmail.com>
2024-04-28 15:13:21 -0300

    hotfix remove bnt t1 on print mmode not appearing

--- src/components/componentesCompartilhados/Formacao.vue
+++ src/components/componentesCompartilhados/Formacao.vue
@@ -140,7 +140,7 @@ export default {
   display: none;
 }
 
-.template1-formacao-container:hover .template1-remove-bnt{
+.template1-formacao-container:hover .template1-remove-bnt {
   display: block;
   background-color: white;
   padding: 10px;
@@ -211,9 +211,14 @@ export default {
   padding: 5px;
 }
 @media print {
-  .template1-remove-bnt{
+  .template1-formacao-container:hover .template1-remove-bnt {
     display: none;
   }
+
+  .template1-remove-bnt{
+    display: none !important;
+  }
+  
   .template2-remove-bnt{
     display: none !important;
   }
