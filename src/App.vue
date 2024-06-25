e46b4ed2957b5bdbb15f070149b1b01b8d910c17
Juliano <juliano_soder@hotmail.com>
2024-06-25 10:35:30 -0300

    fix css circle bug

--- src/App.vue
+++ src/App.vue
@@ -152,11 +152,11 @@
 
     <!-- animated bg -->
     <div class='ripple-background'>
-      <div class='circle xxlarge shade1'></div>
-      <div class='circle xlarge shade2'></div>
-      <div class='circle large shade3'></div>
-      <div class='circle mediun shade4'></div>
-      <div class='circle small shade5'></div>
+      <div class='circle-animated xxlarge shade1'></div>
+      <div class='circle-animated xlarge shade2'></div>
+      <div class='circle-animated large shade3'></div>
+      <div class='circle-animated mediun shade4'></div>
+      <div class='circle-animated small shade5'></div>
     </div>
 </template>
 
@@ -1147,7 +1147,7 @@ body{
 }
 
 
-.circle{
+.circle-animated {
   position: absolute;
   border-radius: 50%;
   background: white;
